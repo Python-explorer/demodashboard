16dd9156fd416106609818aba9403e9b6d4c01a4
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-11 17:54:22 +0000

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 from bokeh.plotting import figure  # Make sure this import is included
 from charts import generate_chart_1  # Import the modular chart function from charts.py
 from bokeh.layouts import gridplot
