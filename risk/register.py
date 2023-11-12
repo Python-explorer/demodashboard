e8086851351873e1d2d7aca9d961bea0e250f13a
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 13:35:49 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -1,19 +1,22 @@
+
 import streamlit as st
 import pandas as pd
-
-# Write the word 'test' to the Streamlit app
-st.markdown("<span style='color: black;'>test</span>", unsafe_allow_html=True)
-st.write('test')
 
 # Raw URL of the CSV file in the GitHub repository
 csv_url = 'https://raw.githubusercontent.com/Python-explorer/demodashboard/main/risk/Risk.csv'
 
-# Load the CSV file into a DataFrame
-df = pd.read_csv(csv_url)
+# Load the CSV file into a DataFrame and ensure proper data types, if necessary
+df = pd.read_csv(csv_url, dtype={"text_column1": str, "text_column2": str, "num_column1": float, "num_column2": float, "num_column3": float})
 
-st.write(df.head(3))
+# Display the DataFrame as a table in Streamlit to debug
+st.write("Displaying the first two rows of the DataFrame:")
+st.write(df.head(2))
+
+# Display the word 'test' with black text color
+st.markdown("<span style='color: black;'>test</span>", unsafe_allow_html=True)
 
 # Display the DataFrame as a table in Streamlit
+st.write("Displaying the full DataFrame:")
 st.table(df)
 
 
