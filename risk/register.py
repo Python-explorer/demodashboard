fb2efa191e717bdd40b045bf60dac7db558a5b24
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 11:02:34 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -8,10 +8,15 @@ def show_risk():
     st.markdown("<h1 style='color: black;'>Risk</h1>", unsafe_allow_html=True)
     st.write("Debug: Entered show_risk_content function")
     st.write("Risk-related content displayed here.")
+
     url = 'https://raw.githubusercontent.com/Python-explorer/demodashboard/main/risk/Risk.csv'
     try:
+        # Read the CSV into a DataFrame
+        df = pd.read_csv(url)
+
         # Display the DataFrame as a table in Streamlit
         st.write(df)
-    show()
 
+    except Exception as e:
+        st.error(f"An error occurred while reading the file: {e}")
  
