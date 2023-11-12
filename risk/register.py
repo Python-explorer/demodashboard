6304a190b2fca017db77e9980da93c5be3763709
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 11:04:10 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd 
 import os
-import streamlit as st
-import pandas as pd
 
 def show_risk():
     st.markdown("<h1 style='color: black;'>Risk</h1>", unsafe_allow_html=True)
@@ -11,12 +9,18 @@ def show_risk():
 
     url = 'https://raw.githubusercontent.com/Python-explorer/demodashboard/main/risk/Risk.csv'
     try:
-        # Read the CSV into a DataFrame
-        df = pd.read_csv(url)
+        # Read the CSV into a DataFrame, specifying the line terminator if necessary
+        df = pd.read_csv(url, lineterminator='\n')
 
-        # Display the DataFrame as a table in Streamlit
+        # Display the first few rows of the DataFrame for debugging
+        st.write("First few rows of the DataFrame:")
+        st.write(df.head())
+
+        # Display the entire DataFrame as a table in Streamlit
+        st.write("Full DataFrame:")
         st.write(df)
 
     except Exception as e:
         st.error(f"An error occurred while reading the file: {e}")
- 
+
+show_risk()
