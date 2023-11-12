5a1fdb8556805e93358fccd55788e9c079d07dba
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 11:15:28 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -1,4 +1,11 @@
 import streamlit as st
 import pandas as pd
 
+def show_risk():
+    url = 'https://raw.githubusercontent.com/Python-explorer/demodashboard/main/risk/Risk.csv'
+    df = pd.read_csv(url)
+    st.write(df)
 
+
+# This call should be at the top-level indentation, not inside any function or conditional block
+show_risk()
