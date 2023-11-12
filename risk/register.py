155d19aef07bede6bf822d1e66af88af22ad6637
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 10:42:07 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -4,7 +4,6 @@ def show_risk_content():
     st.markdown("<h1 style='color: black;'>Risk</h1>", unsafe_allow_html=True)
     st.write("Debug: Entered show_risk_content function")
     # Your code to display the risk content goes here
-    st.markdown("## Risk Dashboard")
     st.write("Risk-related content displayed here.")
     # More Streamlit commands to display data, charts, etc.
  
