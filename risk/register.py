8c3ecccd99ebfff98da810c2e444f7db8be27549
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 13:07:28 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -1,9 +1,24 @@
 import streamlit as st
 import pandas as pd
 
-def show_risk():
-    # Using markdown to display a header
-    st.markdown("# Risk")
+def display_csv_as_table():
+    # URL or path to your CSV file
+    csv_url = 'path_or_url_to_your_csv_file.csv'
 
-# Call the function to display the header
-show_risk()
+    try:
+        # Read the CSV file
+        df = pd.read_csv(csv_url)
+
+        # Display the DataFrame as a table in Streamlit
+        st.write("## Data from CSV File")
+        st.table(df)
+
+    except Exception as e:
+        st.error(f"An error occurred while reading the file: {e}")
+
+def main():
+    st.title("CSV Viewer App")
+    display_csv_as_table()
+
+if __name__ == "__main__":
+    main()
