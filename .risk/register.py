94655b7591a0fdbe8085b0f058a9d31fc07f56bf
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 10:21:36 +0000

    Create register.py

---
+++ .risk/register.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def show_risk_content():
+    # Your code to display the risk content goes here
+    st.markdown("## Risk Dashboard")
+    st.write("Risk-related content displayed here.")
+    # More Streamlit commands to display data, charts, etc.
