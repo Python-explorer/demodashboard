91b0f02d85d739591b74f7050ab8ae81c3533199
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 10:41:15 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 def show_risk_content():
+    st.write("Debug: Entered show_risk_content function")
     # Your code to display the risk content goes here
     st.markdown("## Risk Dashboard")
     st.write("Risk-related content displayed here.")
