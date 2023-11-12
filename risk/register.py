43cbee30c7ab675317a978445bafc08aa47efc18
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 10:46:35 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def show_risk_content():
+def show_risk():
     st.markdown("<h1 style='color: black;'>Risk</h1>", unsafe_allow_html=True)
     st.write("Debug: Entered show_risk_content function")
     # Your code to display the risk content goes here
