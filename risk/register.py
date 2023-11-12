998ae12038ee63aef5fcfb2e36db56f9f6b9f0bf
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 11:14:32 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -1,15 +1,4 @@
 import streamlit as st
 import pandas as pd
 
-def show_risk():
-    url = 'https://raw.githubusercontent.com/Python-explorer/demodashboard/main/risk/Risk.csv'
-    try:
-        # Read the CSV into a DataFrame
-        df = pd.read_csv(url)
-        # Display the DataFrame as a table in Streamlit
-        st.write(df)
-    except Exception as e:
-        st.error(f"An error occurred while reading the file: {e}")
 
-# This call should be at the top-level indentation, not inside any function or conditional block
-show_risk()
