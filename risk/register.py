f061aad563fdfb37d224ae94c36788efd21160ee
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 13:13:05 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -1,23 +1,7 @@
 import streamlit as st
-import pandas as pd
-
-def display_csv_as_table():
-    csv_url = 'https://raw.githubusercontent.com/Python-explorer/demodashboard/main/risk/Risk.csv'  # Ensure this is correct
-
-    try:
-        df = pd.read_csv(csv_url)
-
-        if df.empty:
-            st.write("The DataFrame is empty. Check your CSV file.")
-        else:
-            st.write("## Data from CSV File")
-            st.table(df)
-    except Exception as e:
-        st.error(f"An error occurred while reading the file: {e}")
 
 def main():
-    st.title("CSV Viewer App")
-    display_csv_as_table()
+    st.write("This is a test")
 
 if __name__ == "__main__":
     main()
