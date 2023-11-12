26381a3ce1f1e95ad24ebd821283f4a00a71bddc
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 13:22:48 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
-def main():
-    st.markdown("<span style='color: black;'>This is black text</span>", unsafe_allow_html=True)
-    st.write("This is a test")
-    main()
+# Function to create an HTML string for a blank 5x5 grid
+def create_blank_grid(rows, cols):
+    table = "<table>"
+    for _ in range(rows):
+        table += "<tr>"
+        for _ in range(cols):
+            table += "<td style='border:1px solid black; width: 50px; height: 50px;'></td>"
+        table += "</tr>"
+    table += "</table>"
+    return table
+
+# Create a 5x5 grid
+grid_html = create_blank_grid(5, 5)
+
+# Display the grid in Streamlit
+st.markdown(grid_html, unsafe_allow_html=True)
