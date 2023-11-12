ceb566d8ef442069ce87731131c13fd1a4042011
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 11:06:39 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -3,11 +3,10 @@ import pandas as pd
 import os
 
 def show_risk():
-    st.markdown("<h1 style='color: black;'>Risk</h1>", unsafe_allow_html=True)
     url = 'https://raw.githubusercontent.com/Python-explorer/demodashboard/main/risk/Risk.csv'
     try:
         # Read the CSV into a DataFrame, specifying the line terminator if necessary
-        df = pd.read_csv(url, lineterminator='\n')
+        df = pd.read_csv(url)
         st.write(df)
 
     except Exception as e:
