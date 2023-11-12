b58ceb3e1aadd548626b75473c5684a25b3ea94e
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 10:03:23 +0000

    Update charts.py

--- analysis/charts.py
+++ analysis/charts.py
@@ -8,7 +8,7 @@ def generate_chart_1():
     df = pd.read_csv(url)
     treatment_functions = df['Treatment Function'].loc[df['Treatment Function'] != 'Total']
     data = df.loc[df['Treatment Function'] != 'Total', :].iloc[:, 1:]
-    p = figure(x_range=list(df.columns[1:]), plot_height=250, title="Number of Patients Over Time by Treatment Function", x_axis_label="Month", y_axis_label="Number of Patients")
+    p = figure(x_range=list(df.columns[1:]), plot_height=600, title="Number of Patients Over Time by Treatment Function", x_axis_label="Month", y_axis_label="Number of Patients")
     source = ColumnDataSource(data)
     num_colors = len(treatment_functions)
     color_palette = ["#%06x" % np.random.randint(0, 0xFFFFFF) for _ in range(num_colors)]
