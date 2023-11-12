970d5ae9b677c16c8a1ac0ccfe5dfe72e06354e8
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 13:36:35 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -8,12 +8,12 @@ csv_url = 'https://raw.githubusercontent.com/Python-explorer/demodashboard/main/
 # Load the CSV file into a DataFrame and ensure proper data types, if necessary
 df = pd.read_csv(csv_url, dtype={"text_column1": str, "text_column2": str, "num_column1": float, "num_column2": float, "num_column3": float})
 
+# Display the word 'test' with black text color
+st.markdown("<span style='color: black;'>test</span>", unsafe_allow_html=True)
+
 # Display the DataFrame as a table in Streamlit to debug
 st.write("Displaying the first two rows of the DataFrame:")
 st.write(df.head(2))
-
-# Display the word 'test' with black text color
-st.markdown("<span style='color: black;'>test</span>", unsafe_allow_html=True)
 
 # Display the DataFrame as a table in Streamlit
 st.write("Displaying the full DataFrame:")
