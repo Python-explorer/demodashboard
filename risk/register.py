69b8112b1dd1df2013679f4162ad01e098140ff2
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 11:01:14 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -8,11 +8,10 @@ def show_risk():
     st.markdown("<h1 style='color: black;'>Risk</h1>", unsafe_allow_html=True)
     st.write("Debug: Entered show_risk_content function")
     st.write("Risk-related content displayed here.")
-
     url = 'https://raw.githubusercontent.com/Python-explorer/demodashboard/main/risk/Risk.csv'
     try:
         # Display the DataFrame as a table in Streamlit
         st.write(df)
-    show()
+show()
 
  
